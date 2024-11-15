f83d2a0c5adc9cbb3b357ee80e183c7c07cbd866
Zanyllect58 <andres.galeano@usantoto.edu.co>
2024-11-14 22:58:04 -0500

    correciones del login , register, inicio

--- app.py
+++ app.py
@@ -8,29 +8,29 @@ def inicio():
     return render_template('index.html')
 
 # Rutas relacionadas con el dashboard y la administración
-@app.route('/dashboard')
+@app.route('/dashboard_admin')
 def dashboard():
     return render_template('admin/dashboard_admin.html')
 
-@app.route('/dashboard-usuarios')
+@app.route('/dashboard_usuarios')
 def dashboard_usuarios():
     return render_template('usuario/dashboard_usuarios.html')
 
 # Rutas relacionadas con la gestión de usuarios
-@app.route('/usuarios')
+@app.route('/dashboard_admin_usuarios')
 def users():
     return render_template('admin/dashboard_admin_usuarios.html')
 
-@app.route('/crear-usuario')
+@app.route('/registrar_usuarios')
 def crear_usuario():
     return render_template('registrar.html')  # Ruta correcta si está directamente en templates
 
-@app.route('/editar-usuario')
+@app.route('/editar_usuario')
 def editar_usuario():
     return render_template('admin/dashboard_admin_usuarios_editar_usuarios.html')  # Ruta correcta en la carpeta admin
 
 # Rutas relacionadas con salas
-@app.route('/crear-sala')
+@app.route('/crear_sala')
 def crear_sala():
     return render_template('admin/dashboard_admin_crear_sala.html')
 
